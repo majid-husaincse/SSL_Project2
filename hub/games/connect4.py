f0bbc1de1ff72bce2e4432791dfeadaa22149baf
Majid <majidhusainmmh111@gmail.com>
2026-04-09 00:20:57 +0530

    Completed basic version of Connect4

--- hub/games/connect4.py
+++ hub/games/connect4.py
@@ -1,7 +1,152 @@
-def run(self):
-    pass
+import sys
+import pygame
+import numpy as np
 
-class Connect4:
+#Game defining class
+class Connect4():
+
     def __init__(self, player1, player2):
+        super().__init__()
+        #initialising players
         self.player1 = player1
         self.player2 = player2
+
+        self.turn = 0 #0 is for player1 and 1 is for player2
+
+        #Board-Size:
+        self.ROWS = 6
+        self.COLS = 7
+        self.board = np.zeros((self.ROWS,self.COLS) , dtype = int) #empty board
+
+    def current_player(self):
+        return self.player1 if self.turn ==0 else self.player2
+    
+    def drop(self,col,player):
+        #returns the row where item gets placed
+        for i in [5,4,3,2,1,0]:
+            if self.board[i][col] == 0:
+                self.board[i][col] = player
+                return i
+        return None
+    def switch_turn(self):
+        self.turn = 1 - self.turn     #switching between 0 and 1
+
+    def draw(self,screen):
+        #gray screen and green borders
+        screen.fill((255,255,255))
+        for r in range(self.ROWS):
+            for c in range(self.COLS):
+                #red for player1 blue for player2
+                pygame.draw.rect(screen, (0,255,0), (c*90+50, r*90+50, 90, 90), 5) 
+                if self.board[r][c] == 1:
+                    pygame.draw.rect(screen, (255,0,0), (c*90+55, r*90+55, 80, 80))
+                elif self.board[r][c] == 2:
+                    pygame.draw.rect(screen, (0,0,255), (c*90+55, r*90+55, 80, 80))
+
+    def check_win(self,player,col,row):
+        #win needs to be checked only in the column row or diagonal where last item was placed
+        conseq=0   #conseq for consecutive
+        #checking 4 consecutive in the row
+        for i in range (0,self.COLS):
+            if self.board[row][i] == player:
+                conseq+=1
+            else:
+                conseq=0
+            if conseq == 4:
+                return True
+        conseq=0
+        #checking 4 consecutive in the column
+        for j in range (0,self.ROWS):
+            if self.board[j][col] == player:
+                conseq+=1
+            else:
+                conseq=0
+            if conseq == 4:
+                return True
+        conseq=0
+        #checking Main Diagonal
+        for d in range(-6,6):        # [ -6 to 6 is the farthest we can need]
+            if 0 <= row + d < self.ROWS and 0 <= col + d < self.COLS:
+                if self.board[row + d][col + d] == player:
+                    conseq+=1
+                else:
+                    conseq=0
+            if conseq == 4:
+                return True
+        conseq = 0
+        #checking Anti Diagonal 
+        for d in range(-6,6):
+            if 0 <= row - d < self.ROWS and 0 <= col + d < self.COLS:
+                if self.board[row - d][col + d] == player:
+                    conseq+=1
+                else:
+                    conseq=0
+            if conseq == 4:
+                return True
+            
+        return False
+    
+    def board_full(self):
+        # To check if game is over
+        return 0 not in self.board
+
+    #Main Loop
+    def run(self):
+        pygame.init()
+        clock = pygame.time.Clock()
+        global screen
+        screen = pygame.display.set_mode((800,700))
+        pygame.display.set_caption('Connect FOUR')
+        self.draw(screen)
+        running = True
+
+        while running:
+
+            #To quit
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    return None
+                #User inputs
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_pos = event.pos
+                    col = (mouse_pos[0] - 50)//90
+                    if col > 6 or col < 0:
+                        continue
+                    player = 1 if self.turn == 0 else 2
+                    row = self.drop(col,player)
+                    if row is not None:
+                        if self.check_win(player, col, row):
+                            #if Winner found:
+                            self.draw(screen)
+                            winner = self.current_player()
+                            font = pygame.font.Font(None, 50)
+                            text = font.render(f"{winner} wins", True, ((255,0,0) if player == 1 else (0,0,255)))
+
+                            screen.blit(text, (250, 600))
+                            pygame.display.update()
+                            pygame.time.wait(3000)
+                            return winner
+                        
+                        #move on with next move
+                        self.switch_turn()
+                    if self.board_full():
+
+                        #Board full but no winner...
+                        self.draw(screen)
+                        text_font = pygame.font.Font(None, 50)
+                        tie_text = text_font.render("Match tied", True, (0, 0, 0))
+                        screen.blit(tie_text, (250, 600))
+                        pygame.display.update()
+                        pygame.time.wait(3000)
+                        return "Nobody"
+            self.draw(screen)
+            pygame.display.update()
+            clock.tick(60)
+
+if __name__ == "__main__":
+    player1 = sys.argv[1]
+    player2 = sys.argv[2]
+    game = Connect4(player1, player2)
+    winner = game.run()
+    print( '\n', f"{winner} wins")
